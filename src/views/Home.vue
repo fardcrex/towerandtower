276e7f6280390ea87c8837ad74b4a364b6062ac9
jair <jacobo_9410@hotmail.com>
2020-07-24 16:48:43 -0500

    cambios v0.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <h2>{{latitud}}</h2>
+    <h2>{{longitude}}</h2>
+    <QrcodeStream
+      class="aaa"
+      @decode="onDecode"
+    ></QrcodeStream>
+
+    <QrcodeCapture @decode="onDecode" />
+    <!--    <QrcodeDropZone @decode="onDecode" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { QrcodeStream, QrcodeCapture } from "vue-qrcode-reader";
 export default {
-  name: 'Home',
+  name: "Home",
+  created() {
+    if (navigator.geolocation) {
+      console.log(navigator.geolocation);
+      navigator.geolocation.getCurrentPosition((funcExito) => {
+        console.log(funcExito);
+        this.latitud = funcExito.coords.latitude;
+        this.longitude = funcExito.coords.longitude;
+        console.log("funcExito");
+      });
+    } else {
+      // No hay soporte para la geolocalización: podemos desistir o utilizar algún método alternativo
+    }
+  },
+  data() {
+    return {
+      latitud: 0,
+      longitude: 0,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    QrcodeStream,
+
+    QrcodeCapture,
+  },
+  methods: {
+    onDecode(decodedString) {
+      console.log(decodedString);
+      // ...  console.log(decodedString);
+    },
+  },
+};
 </script>
+<style lang="scss">
+.aaa {
+  width: 100px;
+  height: 100px;
+  color: red;
+}
+</style>
